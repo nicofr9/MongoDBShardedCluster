444074aefa70e00f196fcce9eb12b23ae9c83510
Frongia Nicolò <nicolo.frongia@beta80group.it>
2023-01-10 12:04:33 +0100

    initial commit

---
+++ scripts/initCluster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
+docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
+docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
+docker exec -it shard-01-node-a bash -c "echo 'rs.addArb(\""shard01-x:27017\"")' | mongosh --port 27017"
+docker exec -it shard-02-node-a bash -c "echo 'rs.addArb(\""shard02-x:27017\"")' | mongosh --port 27017"
+sleep(120)
+docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
